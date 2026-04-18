07408032461e057897314663c612ea43b278e677
LeMinhHieu <leminhhieu20042004@gmai.com>
2026-04-18 20:08:09 +0700

    add log out

--- app/src/main/java/com/example/masterai/api/RetrofitClient.java
+++ app/src/main/java/com/example/masterai/api/RetrofitClient.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient {
     // Thay 192.168.1.50 bằng IP thực tế của máy chạy Django (Máy B)
 
-    private static final String BASE_URL = "http://192.168.1.15:8000/";
+    private static final String BASE_URL = "http://192.168.1.11:8000/";
     private static Retrofit retrofit = null;
 
     public static ApiService getApiService() {
